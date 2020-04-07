8e012fcaedae8f3ae2785a13b21b7d46cc152de2
Sydora <artiom.sidora@gmail.com>
2020-04-07 13:41:16 +0300

    Add() method

--- src/LearnToCode/LearnToCode.Collections/MyList.cs
+++ src/LearnToCode/LearnToCode.Collections/MyList.cs
@@ -1,40 +1,109 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LearnToCode.Collections
 {
-    public class MyList<T>
+    public class MyList<T>:IEnumerable
     {
+        T[] _customList = new T[1], _tempList;
+        int _startPosition = -1;
+        int _position = -1;
+        
         public int Count { get; private set; }
 
         public T this[int index]
-        { 
-            get => throw new NotImplementedException(); 
-            set => throw new NotImplementedException(); 
+        {
+            get
+            {
+                return _customList[index];
+            }
+            set
+            {
+                try
+                {
+                    _customList[index] = value;
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            Array.Resize(ref _customList, Count);
+            _position++;
+            _customList[_position] = item;
+            Count++;
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            Count = 0;
+            _position = -1;
+            _customList = new T[1]; 
         }
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < _customList.Length; i++)
+            {
+                if (item.Equals(_customList[i]))
+                    return i;
+            }
+            return -1;
         }
 
         public void InsertAt(int index, T item)
         {
-            throw new NotImplementedException();
+            
         }
 
         public void Remove(T item)
         {
-            throw new NotImplementedException();
+            _tempList = _customList;
+            for(int i = 0; i < _tempList.Length; i++)
+            {
+                if (item.Equals(_tempList[i]))
+                    {
+                        Count--;
+                        break;
+                    }                
+       
+             }
+
+            if (Count.Equals(_tempList.Length))
+            {
+                Console.WriteLine("Error! Item missing.");
+            }
+            else
+            {
+                _customList = new T[Count];
+
+                for (int i = 0; i < _tempList.Length; )
+                    for(int j = 0; j < _customList.Length; j++)
+                {
+                    if (item.Equals(_tempList[i]))
+                    {
+                            j--;
+                            i++;
+                        continue;
+                    }
+                    _customList[j] = _tempList[i];
+                        i++;
+                }
+            }
+
+           
+            
         }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _customList.GetEnumerator();
+        }       
     }
 }
---
+++ src/LearnToCode/LearnToCode.Collections/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnToCode.Collections
+{
+    class Program
+    {
+        public static void Main()
+        {
+            MyList<int> myInt = new MyList<int>(5) {};
+            MyList<string> myString = new MyList<string>(5) { };
+            myInt[0] = 0;
+            myInt[1] = 1;
+            myInt[3] = 3;
+            Console.WriteLine();
+            for (int i = 0; i < myInt.Count; i++)
+            {
+                Console.Write(myInt[i] + " ");
+            }
+            Console.WriteLine();
+
+            myInt.Remove(3);
+            for (int i = 0; i < myInt.Count; i++)
+            {
+                Console.Write(myInt[i] + " ");
+            }
+            Console.WriteLine( );
+            Console.WriteLine(myInt.Count);
+
+
+
+            myString[0] = "zero";
+            myString[1] = "one";
+            myString[3] = "three";
+            Console.WriteLine();
+            for (int i = 0; i < myString.Count; i++)
+            {
+                Console.Write(myString[i] + " ");
+            }
+            Console.WriteLine();
+
+            myString.Remove("three");
+            for (int i = 0; i < myString.Count; i++)
+            {
+                Console.Write(myString[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(myString.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
